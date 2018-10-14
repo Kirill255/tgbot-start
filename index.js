505104435cff9cfaaf64b1718d7db791fb529d31
Kirill255 <jhgbnm1@mail.ru>
2018-10-14 18:09:00 +0300

    create express server

--- index.js
+++ index.js
@@ -1,55 +1,28 @@
-require('dotenv').config();
 
-const fetch = require('node-fetch');
-const HttpsProxyAgent = require('https-proxy-agent');
-const qs = require('querystringify');
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5000
 
-const API = "https://api.telegram.org/bot";
-const PROXY = process.env.PROXY || "http://118.174.211.219:3128"; // прокси бесплатные беру тут https://hidemyna.me/ru/proxy-list/?type=s#list  https://www.sslproxies.org/ , платные прокси можно вынести в конфиг
-let TOKEN = process.env.TOKEN || "";
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// https://core.telegram.org/bots/api#making-requests 
-// https://core.telegram.org/bots/api#available-methods
-// https://api.telegram.org/bot<token>/METHOD_NAME
+app.get("/", function (req, res) {
+    // res.send("GET request to the homepage");
+    res.status(200).end();
+});
 
-const BASE_URL = `${API}${TOKEN}/`; // https://api.telegram.org/bot<token>/
-let url = "";
-let getUpdatesOffset = 0; // id последнего присланного сообщения
+// POST method route
+app.post("/", function (req, res) {
+    res.send("POST request to the homepage");
+});
 
-const sendRequest = async (method, params = {}) => {
+app.use(function (req, res, next) {
+    res.status(404).send("Not found!");
+});
 
-    url = `${BASE_URL}${method}`; // https://api.telegram.org/bot<token>/METHOD_NAME
+app.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).send("Something broke!");
+});
 
-    if (Object.keys(params).length) {
-        let query = qs.stringify(params, true); // ?chat_id=id&text=text
-        url = `${url}${query}`; // https://api.telegram.org/bot<token>/METHOD_NAME?chat_id=id&text=text
-    }
-    console.log('url :', url);
-
-    try {
-        const response = await fetch(url, { agent: new HttpsProxyAgent(PROXY) })
-        const { result } = await response.json();
-        // console.log("result1 :", result);
-
-        if (result.length) {
-            getUpdatesOffset = result[result.length - 1].update_id + 1; // id последнего присланного сообщения + 1, тоесть следующее новое сообщение, которое мы ещё не получали. Мы получили стопку сообщений, взяли id последнего из них, и дальше запрашиваем сообщения только начиная с послднего полученного в прошлый раз + 1 (тоесть начиная со следующего). Нам нужны только новые сообщения, старые мы уже получили
-        }
-
-        return result;
-    } catch (error) {
-        console.log("error: ", error);
-    }
-}
-
-setInterval(() => {
-    sendRequest("getUpdates", { "offset": getUpdatesOffset })
-        .then(result => {
-            // console.log("result2: ", result)
-            result.forEach(item => {
-                // console.log('item :', item);
-                let chat_id = item.message.chat.id;
-                sendRequest("sendMessage", { "chat_id": chat_id, "text": new Date().toLocaleString([], { hour: '2-digit', minute: '2-digit', second: '2-digit' }) })
-            });
-        })
-        .catch(ex => console.log(ex.message));
-}, 10000);
+app.listen(PORT, () => console.log("Server start"));
