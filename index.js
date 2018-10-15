dfd9431fd238f121dc0905630da885703b2c3b3f
Kirill255 <jhgbnm1@mail.ru>
2018-10-15 21:47:44 +0300

    prepare to Heroku deploy

--- index.js
+++ index.js
@@ -1,30 +1,23 @@
 // перед деплоем на хероку нужно не забывать удалять dotenv из зависимостей(dependencies) и закомментировать/удалить эту строчку, инчае будут конфликтовать dotenv из проекта и встроенный на хероку сервере dotenv(или что там используется для чтения env конфига), но для локальной разработки он нам нужен
 // upd: чтобы каждый раз не удалять/устанавливать пакет dotenv, установил его в devDependencies, так как хероку по умолчанию не устанавливает зависимости из этой секции
-require("dotenv").config(); // на сервере heroku есть свой конфиг с переменными, все переменные нужно перенести туда
+// require("dotenv").config(); // на сервере heroku есть свой конфиг с переменными, все переменные нужно перенести туда
 
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
+
+// это временная мера, в следующем обновлении node-telegram-bot-api, это будет не нужно
+process.env.NTBA_FIX_319 = 1;   // fix cancellation of promises https://github.com/yagop/node-telegram-bot-api/issues/319. module.js:652:30
 
 const TelegramBot = require('node-telegram-bot-api');
 
-// const PROXY = process.env.PROXY || "";
 let TOKEN = process.env.TOKEN || "";
 const url = `https://${process.env.HEROKU_APP_NAME}.herokuapp.com`;
-
-// При использовании getUpdates
-// а также в России telegram заблокирован, поэтому нужно использовать proxy
-// const bot = new TelegramBot(TOKEN, {
-//     polling: true,
-//     request: {
-//         proxy: PROXY
-//     },
-// });
 
 // При использовании WebHook
 const bot = new TelegramBot(TOKEN);
 
-bot.setWebHook(`${url}/bot${TOKEN}`);
+bot.setWebHook(`${url}/bot${TOKEN}`, {});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -35,7 +28,7 @@ app.get("/", (req, res) => {
 });
 
 // POST method route
-app.post("/", (req, res) => {
+app.post(`/bot${TOKEN}`, (req, res) => {
     bot.processUpdate(req.body);
     // res.status(200).end();
     res.sendStatus(200);
@@ -53,7 +46,6 @@ app.use((err, req, res, next) => {
 app.listen(PORT, () => console.log("Server start"));
 
 
-
 // Listen for any kind of message. There are different kinds of messages.
 bot.on('message', (msg) => {
     const chatId = msg.chat.id;
@@ -63,33 +55,21 @@ bot.on('message', (msg) => {
 });
 
 
+// error handling
+process.on("uncaughtException", (error) => {
+    let time = new Date();
+    console.log("=== uncaughtException ===");
+    console.log("TIME:", time);
+    console.log("NODE_CODE:", error.code);
+    console.log("MSG:", error.message);
+    console.log("STACK:", error.stack);
+});
 
-/* 
-const sendRequest = async (method, params = {}) => {
-
-    url = `${BASE_URL}${method}`; // https://api.telegram.org/bot<token>/METHOD_NAME
-
-    if (Object.keys(params).length) {
-        let query = qs.stringify(params, true); // ?chat_id=id&text=text
-        url = `${url}${query}`; // https://api.telegram.org/bot<token>/METHOD_NAME?chat_id=id&text=text
-    }
-    console.log("url :", url);
-
-    try {
-        let response = null;
-
-        if (process.env.PROXY) {
-            response = await fetch(url, { agent: new HttpsProxyAgent(PROXY) });
-        } else {
-            response = await fetch(url);
-        }
-
-        const { result } = await response.json();
-        // console.log("result1 :", result);
-
-        return result;
-    } catch (error) {
-        console.log("error: ", error);
-    }
-}
- */
+process.on("unhandledRejection", (error) => {
+    let time = new Date();
+    console.log("=== unhandledRejection ===");
+    console.log("TIME:", time);
+    console.log("NODE_CODE:", error.code);
+    console.log("MSG:", error.message);
+    console.log("STACK:", error.stack);
+});
